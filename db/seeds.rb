60c7f546f0386cb4b8e832fa00fecf88021da561
Arpita-22 <duttaarpita22@gmail.com>
2020-09-09 11:23:42 -0700

    made a sign up method

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,8 @@ student2 = Student.create(name: Faker::Name.name)
 coach1 = Coach.create(name: Faker::Name.name)
 coach2 = Coach.create(name: Faker::Name.name)
 
+sm1.student << student1
+sm1.coach << coach1
+
+
 
