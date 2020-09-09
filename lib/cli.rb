d9ce4c54735397b84fad7cfaa3811b239d5a07c8
mehmehmehlol <hkmeganlo@gmail.com>
2020-09-09 15:29:53 -0700

    added some lines

--- lib/cli.rb
+++ lib/cli.rb
@@ -56,6 +56,7 @@ class CommandLineInterface
         puts "Please enter your name"  
     end
 
+    # Create student if not exist
     def find_or_create_student
         name
         input = gets.chomp
@@ -63,6 +64,7 @@ class CommandLineInterface
         puts "#{@student}"
     end
     
+    # Create coach if not exist
     def find_or_create_coach
         name
         input = gets.chomp
@@ -70,18 +72,22 @@ class CommandLineInterface
         puts "#{@coach}"
     end
 
+    # Return the numbers of swim meet
     def count_swim_meet
         puts "#{SwimMeet.count}"
     end
 
+    # Return the first coach found in the Coach Array
     def first_coach
         puts "#{Coach.first}"
     end
 
+    # Return the last student found in the Student Array
     def last_student
         puts "#{Student.last}"
     end
 
+    # Update coaches name
     def update_coach
         name
         coach = Coach.find_by(name: input)
@@ -89,6 +95,53 @@ class CommandLineInterface
         puts
     end
 
+    # Return the first coach/student/swim meet in the table??
+        # Student.first
+    # Return the last coach/student/swim meet in the table?
+        # Student.last
+
+    # Return the numbers of coaches in that particular swim meet?
+        # Coach.count
+
+    # Return the number of students in that particular swim meet?
+        #  Student.count
+
+    # Return the swim meet that coaches/students belong to
+        # SwimMeet.find_by(name: ??)
+
+    # use the "where" method to select the coaches/students and sort by alphabetical order??
+        # Student.where("name: ??").order(alphabetical order something)
+
+    # Update
+    # Coaches/students can be found, updated and saved
+        # student = Student.create(name: "awesome")
+        # student = Student.find_by(name: "awesome")
+        # student.update(name: "even more awesome")
+        # student.save
+
+    # ^
+    # |
+    # Megan's note: i am not too sure what's the difference between these two.
+    # |
+    # v
+
+    # Can be updated using the update method
+        # Student.create(name: "awesome")
+        # student = student.find_by(name: "awesome")
+        # student.update(name: "awesome")
+
+    # Can update all students at once perhaps? only limited to coaches
+        # update_all their swim meets??
+
+    # Destroy
+    # Coaches/students can delete their own profile
+        # student = student.find_by(name: "awesome")
+        # student.delete
+
+    # I guess we can do coaches can delete all the students and/or swim meet. 
+    # If it's not too complicated, we can send a warning message or something :)
+        # Student.delete_all
+        # SwimMeet.delete_all
 end
  
     
